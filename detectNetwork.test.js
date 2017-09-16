eb98b3ab69a932e91fd4206007b84302bcbff969
RudyRed <willgross94@gmail.com>
2017-09-15 21:41:22 -0400

    before accessing http://chaijs.com/ documentation

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -45,7 +45,6 @@ describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
  
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
@@ -53,7 +52,7 @@ describe('Diner\'s Club', function() {
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('39345678901239') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
  
@@ -64,7 +63,7 @@ describe('American Express', function() {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true. 
   var assert = function(isTrue) {
-    if(isTrue) {
+    if(!isTrue) {
       throw new Error('Test failed');
     }
  
